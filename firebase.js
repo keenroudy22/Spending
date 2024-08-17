003eb7bca6d95518f24c4f04a5f3626ebf9bfb1c
keenroudy22 <kroudebush96@gmail.com>
2024-08-17 09:19:12 -0400

    Update firebase.js

--- firebase.js
+++ firebase.js
@@ -1,12 +1,25 @@
-
-// Import and configure Firebase
+// Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
-import { getFirestore, collection, addDoc } from "firebase/firestore";
+import { getFirestore, collection, addDoc, getDocs } from "firebase/firestore";
+import { getAnalytics } from "firebase/analytics";
 
+// Your web app's Firebase configuration
 const firebaseConfig = {
-    // Your Firebase config object here
+  apiKey: "AIzaSyAWi_XQ5qq-P14FN9V5GdrqgHEhY-B2nNA",
+  authDomain: "spending-902ff.firebaseapp.com",
+  projectId: "spending-902ff",
+  storageBucket: "spending-902ff.appspot.com",
+  messagingSenderId: "424763119988",
+  appId: "1:424763119988:web:8bf244879b296a9dc2f421",
+  measurementId: "G-GWTQLXRLFH"
 };
 
+// Initialize Firebase
 const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
+
+// Initialize Firestore
 const db = getFirestore(app);
-export { db };
+
+// Export Firestore functions
+export { db, collection, addDoc, getDocs };
